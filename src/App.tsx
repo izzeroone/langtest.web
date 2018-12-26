a4d7c9a6b3e081c6928fd99e10a40c4ee8b4f7a6
Pham Hung Vy <vyphamhung10@gmail.com>
2018-12-26 08:24:48 +0700

    l',

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { Button } from 'antd';
   import './App.css';
+import QuestionComponent from './screen/QuizScreenAntd/QuestionComponent';
+import sharedQuizService from './services/QuizService';
 
   class App extends Component {
     render() {
       return (
         <div className="App">
-          <Button type="primary">Button</Button>
+            <QuestionComponent question={sharedQuizService.getQuestion()[0]}></QuestionComponent>
         </div>
       );
     }
---
+++ src/entity/AnswerState.ts
@@ -0,0 +1,1 @@
+export enum AnswerState{'normal', 'selected', 'corrected', 'uncorrected'}
